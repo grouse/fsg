4d70822e60847250f62b887b96149f33f962b901
Jesper Stefansson <jesper.stefansson@gmail.com>
2020-07-29 21:31:21 +0200

    initial implementation and commit

--- external/LLVM/include/clang-c/FatalErrorHandler.h
+++ external/LLVM/include/clang-c/FatalErrorHandler.h
@@ -1,32 +1,3 @@
-/*===-- clang-c/FatalErrorHandler.h - Fatal Error Handling --------*- C -*-===*\
-|*                                                                            *|
-|* Part of the LLVM Project, under the Apache License v2.0 with LLVM          *|
-|* Exceptions.                                                                *|
-|* See https://llvm.org/LICENSE.txt for license information.                  *|
-|* SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception                    *|
-|*                                                                            *|
-\*===----------------------------------------------------------------------===*/
-
-#ifndef LLVM_CLANG_C_FATAL_ERROR_HANDLER_H
-#define LLVM_CLANG_C_FATAL_ERROR_HANDLER_H
-
-#include "clang-c/ExternC.h"
-
-LLVM_CLANG_C_EXTERN_C_BEGIN
-
-/**
- * Installs error handler that prints error message to stderr and calls abort().
- * Replaces currently installed error handler (if any).
- */
-void clang_install_aborting_llvm_fatal_error_handler(void);
-
-/**
- * Removes currently installed error handler (if any).
- * If no error handler is intalled, the default strategy is to print error
- * message to stderr and call exit(1).
- */
-void clang_uninstall_llvm_fatal_error_handler(void);
-
-LLVM_CLANG_C_EXTERN_C_END
-
-#endif
+version https://git-lfs.github.com/spec/v1
+oid sha256:f84ef434fbb6b57c65a56ae1de62cc606f39ea90ebf4482dbdca1d1d9c18a51f
+size 1266
---
+++ site/js/ui.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:22489829128400db36d61c47f4f5ec4108ae770b012d48d5725bcddb87153c89
+size 770
